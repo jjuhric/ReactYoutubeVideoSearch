279733e390509af1b1f76cfbfa7480a9024e3caf
Jeff Uhrick <jeff.uhrick@kikoda.com>
2021-08-09 02:35:56 -0400

    Finished app from course

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,7 +7,9 @@ import youtube from '../apis/youtube';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
-
+    componentDidMount() {
+        this.onFormSubmit('buildings');
+    }
     onVideoSelect = video => {
         this.setState({ selectedVideo: video });
     }
@@ -20,15 +22,26 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+            videos: response.data.items,
+            selectedVideo: response.data.items[0] 
+        });
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onFormSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     };
